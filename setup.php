dadf52164c3ce8fcb2b1abec63fbdcec520040b5
Conrad2098 <48970290+Conrad2098@users.noreply.github.com>
2019-11-04 17:05:28 -0600

    The setup file for the users table

---
+++ setup.php
@@ -0,0 +1,34 @@
+<?php
+
+  $conn = new mysqli('localhost', 'root', '', 'Project');
+  if ($conn->connect_error)
+      die($conn->connect_error);
+
+  $query = "CREATE TABLE users (
+  userID INT,
+  username VARCHAR(32) NOT NULL,
+  password VARCHAR(32) NOT NULL,
+  isAdmin BOOL,
+  PRIMARY KEY (userID)) ENGINE=INNODB;";
+
+  $result = $conn->query($query);
+  if (!$result)
+      die($conn->error);
+
+  $salt1 = "i@e^";
+  $salt2 = "e&j3";
+  
+  $password = "password";
+  
+  $pass = hash('sha512', '$salt1$password$salt2');
+  
+  $query = "INSERT INTO users VALUES(00001, 'username', $pass, false)";
+  $conn->query($query);
+  
+  $adminpassword = "admin";
+  
+  $adminpass = hash('sha512', '$salt1$adminpassword$salt2');
+  
+  $query = "INSERT INTO users VALUES(00000, 'admin', $adminpass, true)"
+  
+?>
