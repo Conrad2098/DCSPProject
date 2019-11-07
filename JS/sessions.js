e2545334943083860d2e0b1682d3316c0931fdb1
Connor Greenhill <connorgreenhill@hotmail.com>
2019-11-07 09:23:44 -0600

    Added logout page
    Error where the javascript is not properly evaluating the cookies/not redirecting to the right page

--- JS/sessions.js
+++ JS/sessions.js
@@ -18,3 +18,44 @@ function login(){
 		alert(err);
 	}
 }
+
+function checkIfLoggedIn(){
+	try{
+		if(document.cookie != null){
+			window.location.replace("./cart.html")
+		}else{
+			window.location.replace("./login.html")
+		}
+	}catch(err){
+		alert(err)
+	}
+}
+
+function loginout(){
+	try{
+		if(document.cookie != null){
+			document.getElementById("account").innerHTML = "Logout";
+		}else{
+			document.getElementById("account").innerHTML = "Login";
+		}
+	}catch(err){
+		alert(err);
+	}
+}
+
+function logout(){
+	try{
+		if(document.cookie != null){
+			document.cookie = null;
+			window.location.replace("./logout.html");
+		}else{
+			window.location.replace("./login.html");
+		}
+	}catch(err){
+		alert(err);
+	}
+}
+
+window.onload = function(){
+	this.loginout();
+}
